05b338f1cd13f487e05242691b7db37172092884
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-06-11 23:49:15 +0600

    Update

--- src/stechbd-cookiecons.php
+++ src/stechbd-cookiecons.php
@@ -28,65 +28,18 @@ if(!defined('ABSPATH'))
 
 // Define the plugin paths.
 
-if(!defined('STCC_PLUGIN'))
-{
-	define('STCC_PLUGIN', plugin_dir_path(__FILE__));
-}
-
-if(!defined('STCC_INC'))
-{
-	define('STCC_INC', STCC_PLUGIN . 'inc/');
-}
-
-if(!defined('STCC_ASSETS'))
-{
-	define('STCC_ASSETS', STCC_PLUGIN . 'assets/');
-}
-
-if(!defined('STCC_ASSETS_CSS'))
-{
-	define('STCC_ASSETS_CSS', STCC_ASSETS . 'css/');
-}
-
-if(!defined('STCC_ASSETS_JS'))
-{
-	define('STCC_ASSETS_JS', STCC_ASSETS . 'js/');
-}
-
-if(!defined('STCC_ASSETS_IMG'))
-{
-	define('STCC_ASSETS_IMG', STCC_ASSETS . 'img/');
-}
-
-if(!defined('STCC_ASSETS_FONTS'))
-{
-	define('STCC_ASSETS_FONTS', STCC_ASSETS . 'fonts/');
-}
-
-if(!defined('STCC_ASSETS_VENDOR'))
-{
-	define('STCC_ASSETS_VENDOR', STCC_ASSETS . 'vendor/');
-}
-
-if(!defined('STCC_ASSETS_VENDOR_CSS'))
-{
-	define('STCC_ASSETS_VENDOR_CSS', STCC_ASSETS_VENDOR . 'css/');
-}
-
-if(!defined('STCC_ASSETS_VENDOR_JS'))
-{
-	define('STCC_ASSETS_VENDOR_JS', STCC_ASSETS_VENDOR . 'js/');
-}
-
-if(!defined('STCC_ASSETS_VENDOR_IMG'))
-{
-	define('STCC_ASSETS_VENDOR_IMG', STCC_ASSETS_VENDOR . 'img/');
-}
-
-if(!defined('STCC_ASSETS_VENDOR_FONTS'))
-{
-	define('STCC_ASSETS_VENDOR_FONTS', STCC_ASSETS_VENDOR . 'fonts/');
-}
+define('STCC_PLUGIN', plugin_dir_path(__FILE__));
+const STCC_INC = STCC_PLUGIN . 'inc/';
+const STCC_ASSETS = STCC_PLUGIN . 'assets/';
+const STCC_ASSETS_CSS = STCC_ASSETS . 'css/';
+const STCC_ASSETS_JS = STCC_ASSETS . 'js/';
+const STCC_ASSETS_IMG = STCC_ASSETS . 'img/';
+const STCC_ASSETS_FONTS = STCC_ASSETS . 'fonts/';
+const STCC_ASSETS_VENDOR = STCC_ASSETS . 'vendor/';
+const STCC_ASSETS_VENDOR_CSS = STCC_ASSETS_VENDOR . 'css/';
+const STCC_ASSETS_VENDOR_JS = STCC_ASSETS_VENDOR . 'js/';
+const STCC_ASSETS_VENDOR_IMG = STCC_ASSETS_VENDOR . 'img/';
+const STCC_ASSETS_VENDOR_FONTS = STCC_ASSETS_VENDOR . 'fonts/';
 
 require_once STCC_INC . 'Main.php';
 
