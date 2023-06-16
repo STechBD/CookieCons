3c5f2d2e8b030b80656d437d75ea5fc6f20375ce
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-06-16 20:08:50 +0600

    Update

---
+++ src/inc/Frontend/Notice.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * Plugin Name: CookieCons by STechBD.Net
+ * Plugin URI: https://project.stechbd.net/cookiecons/
+ * Description: Socialite is a powerful WordPress plugin that allows users to integrate social media login.
+ * Version: 1.0.0
+ * Version Code: 1
+ * Author: S Technologies Limited
+ * Author URI: https://www.stechbd.net
+ * Author Email: product@stechbd.net
+ * Created: June 8, 2023
+ * Updated: June 8, 2023
+ */
+
+namespace STechBD\CookieCons\Frontend;
+
+/**
+ * The frontend notice class file.
+ */
+class Notice
+{
+	// To be added later.
+}
